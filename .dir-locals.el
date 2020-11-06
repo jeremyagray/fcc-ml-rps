d3f9462697986c342236c425f23c8714c915ad66
Jeremy A Gray <jeremy.a.gray@gmail.com>
2020-11-06 09:14:24 -0600

    chore: add editor local configuration
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((python-mode
+  (flycheck-python-flake8-executable . "/home/gray/.virtualenvs/fcc-ml-rps/bin/flake8")
+  (flycheck-python-pylint-executable . "/home/gray/.virtualenvs/fcc-ml/bin/flake8")))
